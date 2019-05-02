7c88561bbd79c37a23641fdb0744939310499828
Out of Ink SOftware <moshekrum@gmail.com>
2019-05-03 01:00:51 +0300

    routine updating

--- webpack.config.js
+++ webpack.config.js
@@ -17,19 +17,20 @@ const manifest = new WebpackPwaManifest({
     description: 'If you are color blind, this is not the game for you.',
     background_color: '#ffffff',
     theme_color: "#ffffff",
-    display: "fullscreen",
+    display: "standalone",
     orientation: "portrait",
     ios: true,
     icons: [{
-            src: path.resolve('./src/icons/192.png'),
-            sizes: [192],
+            src: path.resolve('./src/icons/icon.png'),
+            sizes: [120, 152, 167, 180, 1024],
+            destination: path.join('icons', 'ios'),
             ios: true
-        },
-        {
-            src: path.resolve('./src/icons/512.png'),
-            sizes: [512],
-            ios: true
-        }
+          },
+          {
+            src: path.resolve('./src/icons/icon.png'),
+            sizes: [36, 48, 72, 96, 144, 192, 512],
+            destination: path.join('icons', 'android')
+          }
     ]
 })
 
@@ -47,5 +48,5 @@ module.exports = {
             loader: "babel-loader"
         }]
     },
-    plugins: [html, manifest, offline]
+    plugins: [html, offline, manifest]
 };
